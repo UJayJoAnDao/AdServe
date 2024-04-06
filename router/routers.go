41450fe3c1f5afb0b6f96b610b3384d8836960c6
UJay <plmoknijb1234510@gmail.com>
2024-04-06 15:59:27 +0800

    "feat:實作routes     API入口為host/api/v1/ad"

--- router/routers.go
+++ router/routers.go
@@ -1,3 +1,21 @@
 package routes
 
-import ()
+import (
+	"api/controller"
+
+	"github.com/gorilla/mux"
+)
+
+func SetRouter() *mux.Router {
+	router := mux.NewRouter()
+	
+	// 定義共同的 URL 前綴
+    apiV1 := router.PathPrefix("/api/v1").Subrouter()
+    ad := apiV1.PathPrefix("/ad").Subrouter()
+
+    // 定義路由
+    ad.HandleFunc("/", controller.GetAll).Methods("GET")
+    ad.HandleFunc("/", controller.Create).Methods("POST")
+
+    return router
+}
