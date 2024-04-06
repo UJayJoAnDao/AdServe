dee95f9cd78b85d852df11f83a8879d7dc41b72b
UJay <plmoknijb1234510@gmail.com>
2024-04-06 18:12:21 +0800

    "feat:新增廣告的type、對資料庫的寫入與讀取"

--- model/model.go
+++ model/model.go
@@ -1,1 +1,24 @@
 package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Ad struct {
+	Id         uint        `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
+	Title      string      `json:"title" binding:"required"`
+	StartAt    time.Time   `json:"startAt" binding:"required"`
+	EndAt      time.Time   `json:"endAt" binding:"required"`
+	Conditions AdCondition `json:"conditions"`
+	gorm.Model
+}
+
+type AdCondition struct {
+	AgeStart int      `json:"ageStart"`
+	AgeEnd   int      `json:"ageEnd"`
+	Gender   string   `json:"gender"`
+	Country  []string `json:"country"`
+	Platform []string `json:"platform"`
+}
--- services/service.go
+++ services/service.go
@@ -1,1 +1,25 @@
 package services
+
+import (
+	"api/model"
+	"api/sql"
+)
+
+func GetALlAd() (ads []*model.Ad, err error) {
+	err = sql.Connect.Find(&ads).Error
+	// Error是GORM庫的一個方法，它將返回一個錯誤，如果有錯誤，我們將在控制器中處理這個錯誤。
+	// sql.Connect.Find(&ads)是GORM庫的一個方法，它將從數據庫中獲取所有的廣告，並將它們存儲在一個切片中。
+	return
+}
+
+func CreateAd(ad *model.Ad) (err error) {
+	err = sql.Connect.Create(ad).Error
+	// sql.Connect.Create(ad)是GORM庫的一個方法，它將創建一個新的廣告並將其保存到數據庫中。
+	return
+}
+
+func SearchAd(condition *model.AdCondition) (ads []*model.Ad, err error) {
+	err = sql.Connect.Where(condition).Find(&ads).Error
+	// sql.Connect.Where(condition).Find(&ads)是GORM庫的一個方法，它將從數據庫中獲取符合條件的廣告，並將它們存儲在一個切片中。
+	return
+}
